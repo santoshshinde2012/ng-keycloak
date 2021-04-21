a6e01577ea684a9a78c675a1c70776e8a27f788b
neosoft <neosoft@neosofts-MacBook-Pro.local>
2021-04-21 18:37:30 +0530

    remove unused

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
